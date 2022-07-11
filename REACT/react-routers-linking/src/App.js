7a4276fe93b8c1db17fb6d4825a7de70dded6e9a
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2022-07-11 12:29:43 +0530

    Update App.js

--- REACT/react-routers-linking/src/App.js
+++ REACT/react-routers-linking/src/App.js
@@ -3,18 +3,20 @@ import Shop from './Shop';
 import About from './About';
 import Nav from './Nav';
 import Home from './Home';
+import Footer from './Footer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Nav />
+      <div className="App"> 
+        <Nav /> 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/shop" element={<Shop />} />
         </Routes>
+        <Footer /> 
       </div>
     </Router>
   );
