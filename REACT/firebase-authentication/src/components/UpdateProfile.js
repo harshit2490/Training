41c2408dbad3bfbbfc01ca588fd2dc5b23ea5a04
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2022-07-18 16:00:05 +0530

    Update UpdateProfile.js

--- REACT/firebase-authentication/src/components/UpdateProfile.js
+++ REACT/firebase-authentication/src/components/UpdateProfile.js
@@ -3,6 +3,7 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 
+
 function UpdateProfile() {
     const emailRef = useRef();
     const passwordRef = useRef();
@@ -12,25 +13,24 @@ function UpdateProfile() {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
+
     function handleSubmit(e) {
         e.preventDefault();
         if (passwordRef.current.value !== passwordConfirmRef.current.value) {
             return setError("Passwords do not match");
         }
-
         const promises = [];
         setLoading(true);
         setError("");
-
         if (emailRef.current.value !== currentUser.email) {
             promises.push(updateEmail(emailRef.current.value));
         }
-
         if (passwordRef.current.value) {
             promises.push(updatePassword(passwordRef.current.value));
         }
 
-        Promise.all(promises)
+
+        Promise.all(promises)  // It can execute a code block until a non-block async request is complete.
             .then(() => {
                 navigate("/");
             })
@@ -52,49 +52,46 @@ function UpdateProfile() {
         // setLoading(false);
     }
 
+
     return (
         <>
             <Card>
                 <Card.Body>
                     <h2 className="text-center mb-4">Update Profile</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
-                   
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control
-                                defaultValue={currentUser.email}
                                 type="email"
                                 ref={emailRef}
-                                required
+                                defaultValue={currentUser.email}
                             />
                         </Form.Group>
-                      
+
                         <Form.Group id="password">
                             <Form.Label>Password</Form.Label>
                             <Form.Control
-                                placeholder="Leave it blank to keep the same"
                                 type="password"
                                 ref={passwordRef}
-                                required
+                                placeholder="Leave blank to keep the same"
                             />
                         </Form.Group>
-                       
-                        <Form.Group id="password">
+
+                        <Form.Group id="password-confirm">
                             <Form.Label>Confirm Password</Form.Label>
                             <Form.Control
-                                placeholder="Leave it blank to keep the same"
                                 type="password"
                                 ref={passwordConfirmRef}
-                                required
+                                placeholder="Leave blank to keep the same"
                             />
                         </Form.Group>
-                       
+
                         <Button disabled={loading} className="w-100" type="submit">
-                            Update Profile
+                            Update
                         </Button>
+
                     </Form>
-               
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
