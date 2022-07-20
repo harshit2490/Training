840b26b944ed9b76deb6c45523dbf952c9e940ea
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2022-07-20 18:10:45 +0530

    Add files via upload

---
+++ REACT/02_REDUX/02_redux-demo-flow/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+function App() {
+  const counter = useSelector((state) => state.counter);    // select counter.js state from reducer
+  const isLogged = useSelector((state) => state.isLogged);  // select logged.js state from reducer 
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>Counter : {counter}</h4>
+      </header>
+      <button onClick={() => dispatch(increment(1))}>+</button>
+      <button onClick={() => dispatch(decrement(1))}>-</button>
+      
+      {isLogged ? <h3>Valuable Information</h3> : ""}
+    </div>
+  );
+}
+
+export default App;
