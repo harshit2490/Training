4a35cf1afa26674ed558c1182b88a8135ddf0b63
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2022-08-02 16:17:58 +0530

    Add files via upload

---
+++ REACT/02_REDUX/03_redux-thung-tutorial/src/redux/actions/productActions.js
@@ -0,0 +1,40 @@
+import fakeStoreApi from "../../apis/fakeStoreApi";
+import { ActionTypes } from "../constants/action-types";
+
+
+// Defining all the product actions...
+export const fetchProducts = () =>
+    async function (dispatch) {
+        const response = await fakeStoreApi.get('/products');
+        dispatch({ type: ActionTypes.FETCH_PRODUCTS, payload: response.data })
+    };
+
+
+export const fetchProduct = (id) =>
+    async function (dispatch) {
+        const response = await fakeStoreApi.get(`/products/${id}`);
+        dispatch({ type: ActionTypes.SELECTED_PRODUCT, payload: response.data })
+    };
+
+
+// export const setProducts = (products) => {
+//     return {
+//         type: ActionTypes.SET_PRODUCTS,
+//         payload: products,
+//     };
+// };
+
+
+// export const selectedProduct = (product) => {
+//     return {
+//         type: ActionTypes.SELECTED_PRODUCT,
+//         payload: product,
+//     };
+// };
+
+
+export const removeSelectedProduct = () => {
+    return {
+        type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+    };
+};
