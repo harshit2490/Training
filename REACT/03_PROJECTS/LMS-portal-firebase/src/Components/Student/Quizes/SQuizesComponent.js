c3b65b4d7e740bfcae104a0b4e609476dc3e100d
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2022-08-23 02:05:57 +0530

    Add files via upload

---
+++ REACT/03_PROJECTS/LMS-portal-firebase/src/Components/Student/Quizes/SQuizesComponent.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { getAllCourses, useAuth } from '../../../Contexts/AuthContext';
+
+
+function SQuizesComponent() {
+    const { currentUser, logout } = useAuth();
+    const navigate = useNavigate();
+    const [allCourses, setAllCourses] = useState([]);
+
+    // Fetch all student details...
+    useEffect(() => {
+        async function fetchAllCourses() {
+            let every = await getAllCourses();
+            setAllCourses(every);
+        }
+        fetchAllCourses();
+    }, [currentUser, navigate]);
+    console.log(allCourses)
+
+    return (
+        <div>
+            <div className='my-2 d-flex text-align-center justify-content-center'>
+                <h1>Create <span className='text-primary'>Quizes</span></h1>
+            </div>
+            <hr className="w-75 m-auto background shadow" style={{ padding: "1px" }} />
+
+            {/* ----------{Render Quizes Details}----------- */}
+            {/* {allCourses && allCourses.map((res) => {
+                return (
+                    <>
+                        <h2>{res.title}</h2>
+                        <p>{res.duration}</p>
+                    </>
+                )
+            })} */}
+        </div>
+    )
+}
+
+export default SQuizesComponent;
---
+++ REACT/03_PROJECTS/LMS-portal-firebase/src/Components/Teacher/Sidebar/Sidebar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Sidebar.css';
+
+function Sidebar() {
+
+    return (
+        <div className='main-container d-flex'>
+            <div className='sidebar' id='side_nav'>
+                <div className="header-box b-block d-md-none px-3 pt-3 pb-4">
+                </div>
+                <ul className='list-unstyled px-2'>
+                    <li className="px-3 mt-4 d-block">
+                        <Link to=""
+                            style={{ textDecoration: 'none', color: 'white' }} >
+                            <i className='fa fa-home mx-2'></i>
+                            Dashbord
+                        </Link>
+                    </li>
+                    <hr className='h-color mx-2' />
+                    <li className="px-3 py-2 d-block">
+                        <Link to=""
+                            style={{ textDecoration: 'none', color: 'white' }} >
+                            <i className='fa fa-list-ul mx-2'></i>
+                            Courses
+                        </Link>
+                    </li>
+                    <hr className='h-color mx-2' />
+                    <li className="px-3 py-2 d-block">
+                        <Link to=""
+                            style={{ textDecoration: 'none', color: 'white' }} >
+                            <i class="fa fa-users mx-2"></i>
+                            Students
+                        </Link>
+                    </li>
+                    <hr className='h-color mx-2' />
+                    <li className="px-3 py-2 d-block">
+                        <Link to=""
+                            style={{ textDecoration: 'none', color: 'white' }} >
+                            <i class="fa fa-question-circle-o mx-2" />
+                            Quizes
+                        </Link>
+                    </li>
+                    <hr className='h-color mx-2' />
+                    <li className="px-3 py-2 d-block">
+                        <Link to=""
+                            style={{ textDecoration: 'none', color: 'white' }} >
+                            <i class="fa fa-user-circle-o mx-2"></i>
+                            Profile
+                        </Link>
+                    </li>
+                </ul>
+                <hr className='h-color mx-2' />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
