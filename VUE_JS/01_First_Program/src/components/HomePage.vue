2a0bc3f65f2e9129aa6ad23cd1c3c23eb4d42c39
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2022-10-10 20:02:57 +0530

    Add files via upload

---
+++ VUE_JS/01_First_Program/src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello from Home Component</h1>
+    <h2>{{ propsData }}</h2>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: "HomePage",
+  props: {
+    propsData: String,
+  },
+};
+</script>
