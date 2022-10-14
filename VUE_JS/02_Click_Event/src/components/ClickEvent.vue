591d9e8c442a07ab91e49430a6badf8a27752261
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2022-10-14 11:34:01 +0530

    Add files via upload

---
+++ VUE_JS/02_Click_Event/src/components/ClickEvent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button v-on:click="hello">Click Event</button>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: "ClickEvent",
+  methods: {
+    // Method: For defining any function
+    hello() {
+      console.log("using click event");
+    },
+  },
+};
+</script>
