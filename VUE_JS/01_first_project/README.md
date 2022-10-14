c1ef4870e4285ff2024354a402bfbb7cf228196e
Harshit Singh <45359928+harshit2490@users.noreply.github.com>
2022-10-14 11:51:22 +0530

    Add files via upload

---
+++ VUE_JS/01_first_project/README.md
@@ -0,0 +1,26 @@
+# first_project
+```
+https://www.youtube.com/watch?v=5RgiKA0xOOY&list=PL8p2I9GklV45qwTH-mdzllUuFRJO-euYn&index=4
+```
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ VUE_JS/02_click_event/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ClickEvent />
+  </div>
+</template>
+
+<script>
+import ClickEvent from "./components/ClickEvent.vue";
+
+export default {
+  name: "App",
+  components: {
+    ClickEvent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
